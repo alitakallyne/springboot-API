8e140976793dc0a5580fbda1391cc171a350455f
alita_kallyne <alytakallyne@gmail.com>
2024-01-19 10:36:30 -0300

    feat: created Controller

---
+++ src/main/java/com/example/springboot/Controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.example.springboot.Controller;
+
+import com.example.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+}
