bfb7c8bc43a949c5e153b87956ac9c49121b3ef2
alita_kallyne <alytakallyne@gmail.com>
2024-01-19 10:58:34 -0300

    feat: created post method

--- src/main/java/com/example/springboot/Controller/ProductController.java
+++ src/main/java/com/example/springboot/Controller/ProductController.java
@@ -1,7 +1,15 @@
 package com.example.springboot.Controller;
 
+import com.example.springboot.DTO.ProductRecordDto;
+import com.example.springboot.Model.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +17,12 @@ public class ProductController {
 
     @Autowired
     ProductRepository productRepository;
+
+    @PostMapping("/product/")
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+
+        var productModel = new ProductModel();
+        BeanUtils.copyProperties(productRecordDto,productModel);
+        return  ResponseEntity.status(HttpStatus.created).body(productRepository.save(productModel));
+    }
 }
